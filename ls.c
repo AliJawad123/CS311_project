6a6e339a599254da09f06128433451cbc7010984
AliJawad123 <96284750+AliJawad123@users.noreply.github.com>
2022-11-22 02:34:24 +0500

    Final code of ls -n command

---
+++ ls.c
@@ -0,0 +1,122 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+//#include<string.h>
+
+int counter=1;
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+ls(char *path,int checker)
+{
+printf("path = %s\n",path);
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_DEVICE:
+  // displays the content of file
+  case T_FILE:
+    printf("!!! %s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf("@@@ ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }  
+                               // name, info regarding file,id, size
+      if(checker==0)
+      printf("$$$ %s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+   
+    if(checker==1)
+    printf("%d %s %d %d %d\n",counter++, fmtname(buf), st.type, st.ino, st.size);
+    
+    }
+    break;
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+ int i;
+printf("Hello jawad%%% Ali\n");
+
+int checker=0;
+if(argc>1)
+{
+if(!strcmp(argv[1], "-n"))
+{printf("n available\n");
+checker=1;}
+} 
+  if(argc < 2){
+    ls(".",checker);
+    printf("if ran \n");
+    exit(0);
+  }
+  
+    if(argc < 3 && (!strcmp(argv[1], "-n"))){
+    ls(".",checker);
+    printf("-n ran \n");
+    exit(0);
+  }
+  
+  
+  for(i=1; i<argc; i++)
+  {
+  /*
+  if(!strcmp(argv[1], "-n") && (argc<3))
+  {ls(".",checker);
+  continue;}
+  */
+    ls(argv[i],checker);
+    }
+  exit(0);
+}
+
