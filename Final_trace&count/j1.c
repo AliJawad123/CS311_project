ad861250fdec3a6fc29e9e4e4fab68e2907c4da9
AliJawad123 <96284750+AliJawad123@users.noreply.github.com>
2022-12-04 23:35:00 +0500

    These are the final files of trace and get_count system calls

---
+++ Final_trace&count/j1.c
@@ -0,0 +1,16 @@
+#include "/home/jawad/xv6-riscv/kernel/types.h"
+#include "/home/jawad/xv6-riscv/kernel/stat.h"
+#include "user.h"
+#include <stddef.h>
+
+int main(void)
+{
+
+printf("hello ran from j1 and trace gave %d = \n",trace("/home/jawad/xv6-riscv/file1.txt"));
+open("/home/jawad/xv6-riscv/file1.txt",0);
+open("/home/jawad/xv6-riscv/file1.txt",0);
+open("/home/jawad/xv6-riscv/file1.txt",0);
+printf("value of getcount is = %d\n",getcount());
+
+exit(0);
+}
